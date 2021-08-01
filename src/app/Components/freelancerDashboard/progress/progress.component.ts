aa127c23be65259db2d0275f1540d8079bf2265f
hader zayed <haderzayed55@gmail.com>
2021-08-01 22:51:23 +0200

    upduate catagory

---
+++ src/app/Components/freelancerDashboard/progress/progress.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.css']
+})
+export class ProgressComponent implements OnInit {
+
+  constructor() { }
+  data: any;
+
+  ngOnInit(): void {
+
+  this.data = {
+    datasets: [{
+        data: [
+            11,
+            16,
+            7,
+            3,
+            14
+        ],
+        backgroundColor: [
+            "#FF6384",
+            "#4BC0C0",
+            "#FFCE56",
+            "#E7E9ED",
+            "#36A2EB"
+        ],
+        label: 'My dataset'
+    }],
+    labels: [
+        "Red",
+        "Green",
+        "Yellow",
+        "Grey",
+        "Blue"
+    ]
+
+  }
+
+  }
+}
+
+
