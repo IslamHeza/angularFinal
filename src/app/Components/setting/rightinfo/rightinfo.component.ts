a856034ef8ee973c6d6418086d5533f226816089
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-08-03 02:09:25 +0200

    editprofile

--- src/app/Components/setting/rightinfo/rightinfo.component.ts
+++ src/app/Components/setting/rightinfo/rightinfo.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RightinfoComponent implements OnInit {
 
   constructor() { }
-  values:string=""
+  values=["php","html"]
   ngOnInit(): void {
   }
 }
