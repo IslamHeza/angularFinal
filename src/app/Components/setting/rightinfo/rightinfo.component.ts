2fb37811523e1431b62097ed9ac41190c86dd732
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-08-05 01:48:54 +0200

    delete comment which view data of all user big mistake on update

--- src/app/Components/setting/rightinfo/rightinfo.component.ts
+++ src/app/Components/setting/rightinfo/rightinfo.component.ts
@@ -9,7 +9,6 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./rightinfo.component.css']
 })
 export class RightinfoComponent implements OnInit {
-  // allUsers:any=[];
   user = new User;
   data:any;
   constructor(private userService:UserService,private router:Router,private route:ActivatedRoute) { }
@@ -18,20 +17,12 @@ export class RightinfoComponent implements OnInit {
       this.data = res;
       this.user = this.data;
     })
-    // this.getAllUsers();
     console.log(this.route.snapshot.params.id);
   }
-  // getAllUsers(){
-  //   return this.userService.getAllUsers().subscribe(res => {
-  //     // console.log(res);
-  //     this.allUsers = res;
-  //   })
-  // }
   updateUser(){
     this.userService.updateUser(this.route.snapshot.params.id,this.user).subscribe(res =>{
       console.log(this.user);
       return this.router.navigate(["/profile"]);
-
     })
   }
 }
