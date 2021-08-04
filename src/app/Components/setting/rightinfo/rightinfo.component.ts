c32aa1bfb03ba701e8b09c8b437cd2b474969941
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-08-05 01:47:21 +0200

    edit data

--- src/app/Components/setting/rightinfo/rightinfo.component.ts
+++ src/app/Components/setting/rightinfo/rightinfo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/service/user.service';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-rightinfo',
@@ -6,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rightinfo.component.css']
 })
 export class RightinfoComponent implements OnInit {
-
-  constructor() { }
-  values=["php","html"]
+  // allUsers:any=[];
+  user = new User;
+  data:any;
+  constructor(private userService:UserService,private router:Router,private route:ActivatedRoute) { }
   ngOnInit(): void {
+    this.userService.getUser(this.route.snapshot.params.id).subscribe(res =>{
+      this.data = res;
+      this.user = this.data;
+    })
+    // this.getAllUsers();
+    console.log(this.route.snapshot.params.id);
+  }
+  // getAllUsers(){
+  //   return this.userService.getAllUsers().subscribe(res => {
+  //     // console.log(res);
+  //     this.allUsers = res;
+  //   })
+  // }
+  updateUser(){
+    this.userService.updateUser(this.route.snapshot.params.id,this.user).subscribe(res =>{
+      console.log(this.user);
+      return this.router.navigate(["/profile"]);
+
+    })
   }
 }
