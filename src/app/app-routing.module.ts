a91a1e4bd49050a71d08f742a7dc1cfad199bdde
NermeenShehab <Nermeenshehab490@gmail.com>
2021-08-28 15:56:53 +0200

    add auth to add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -74,7 +74,7 @@ const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'addproject', component: CreateComponent },
+  { path: 'addproject', component: CreateComponent ,canActivate: [AuthGuard]},
   { path: 'listproject', component: ListComponent },
   { path: 'editproject/:id', component: EditComponent },
   { path: 'viewproject/:id', component: ViewComponent ,canActivate: [AuthGuard] },
